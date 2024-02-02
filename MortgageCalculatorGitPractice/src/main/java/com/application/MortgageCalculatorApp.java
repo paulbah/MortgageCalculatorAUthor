a57a8996e33556f87f057977e8681724d65902fd
paulbah <paul4yesus@yahoo.com>
2024-02-02 17:22:14 -0500

    Adding main method and print statement

--- MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -6,5 +6,9 @@ package com.application;
 
 
 public class MortgageCalculatorApp {
-
+public static void main(String[] args) {
+	System.out.println("Mortgage calculator application is starting");
+}
+	
+	
 }
